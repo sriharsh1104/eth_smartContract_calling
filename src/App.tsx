93a8b9e7fa010568b4fff32c8fd4876ea36d0350
sriharsh1104 <prashantsriharsh1104@gmail.com>
2024-05-02 11:50:09 +0530

    intial_setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+const App: React.FC = () => {
+ const [isMetamaskInstalled, setIsMetamaskInstalled] = useState<boolean>(false);
+ const [ethereumAccount, setEthereumAccount] = useState<string | null>(null);
+ 
+ useEffect(() => {
+   if((window as any).ethereum){
+     //check if Metamask wallet is installed
+     setIsMetamaskInstalled(true);
+   }
+ },[]);
+ 
+ //Does the User have an Ethereum wallet/account?
+ async function connectMetamaskWallet(): Promise<void> {
+   //to get around type checking
+   (window as any).ethereum
+     .request({
+         method: "eth_requestAccounts",
+     })
+     .then((accounts : string[]) => {
+       setEthereumAccount(accounts[0]);
+     })
+     .catch((error: any) => {
+         alert(`Something went wrong: ${error}`);
+     });
+ }
+ 
+ if (ethereumAccount === null) {
+   return (
+     <div className="App App-header">
+       {
+         isMetamaskInstalled ? (
+           <div>
+             <img src={logo} alt="logo" />
+             <button onClick={connectMetamaskWallet}>Connect Your Metamask Wallet</button>
+           </div>
+         ) : (
+           <p>Install Your Metamask wallet</p>
+         )
+       }
+ 
+     </div>
+   );
+ }
+ 
+ 
+ return (
+   <div className="App">
+     <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+       <p>
+         ETH wallet connected as: {ethereumAccount}
+       </p>
+     </header>
+   </div>
+ );
 }
-
+ 
 export default App;
